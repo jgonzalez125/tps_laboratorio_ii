4c235aea40549a1ac5a139a83f511bdb2ca0f804
Juan Pablo Gonzalez Conti <juan.g.conti@gmail.com>
2021-10-28 20:30:01 -0300

    Add TP1 again

--- RecuperatoriosTP/TP1/Entidades/Operando.cs
+++ RecuperatoriosTP/TP1/Entidades/Operando.cs
@@ -6,18 +6,17 @@ using System.Threading.Tasks;
 
 namespace Entidades
 {
-    public class Operando 
+    public class Operando
     {
         private double numero;
 
 
         /// <summary>
-        /// Constructor que inicializa en 0 a numero reutilizando 
-        /// el constructor que toma valor por parametro
+        /// Constructor que inicializa en 0 a numero
         /// </summary>
-        public Operando() : this(0)
+        public Operando()
         {
-            new Calculadora();
+            this.numero = 0;
         }
 
         /// <summary>
@@ -30,44 +29,19 @@ namespace Entidades
                 this.numero = ValidarOperando(value);
             }
         }
-
-        /// <summary>
-        /// Constructor que asigna el valor de <paramref name="numero"/>
-        /// a la propiedad <see cref="numero"/>
-        /// </summary>
-        /// <param name="numero"></param>
         public Operando(double numero)
         {
             this.numero = numero;
         }
 
-        /// <summary>
-        /// Constructor que asigna el valor de <paramref name="numero"/>
-        /// utilizando <see cref="Numero"/>
-        /// </summary>
-        /// <param name="numero"></param>
-        public Operando(string numero) 
+        public Operando(string numero)
         {
             this.Numero = numero;
         }
-
-
-        /// <summary>
-        /// Metodo que convierte <param name="numero"></param> a binario
-        /// </summary>
-        /// <param name="numero"></param>
-        /// <returns></returns>
         public static string DecimalBinario(double numero)
         {
             return Convert.ToString((int)numero, 2);
         }
-
-        /// <summary>
-        /// Metodo que convierte <param name="numero"></param> a binario
-        /// </summary>
-        /// <param name="numero"></param>
-        /// <returns>el numero convertido a binario si pudo convertir, si hay <see cref="OverflowException"/> retorna "Limite excedido"
-        /// y si hay <see cref="FormatException"/> retorna "El valor no es valido"</returns>
         public static string DecimalBinario(string numero)
         {
             try
@@ -84,12 +58,6 @@ namespace Entidades
             }
         }
 
-
-        /// <summary>
-        /// Metodo que convierte <param name="numero"></param> a decimal
-        /// </summary>
-        /// <param name="numero"></param>
-        /// <returns>el valor convertido a decimal, si no lo puede convertir, retorna "Valor Invalido"</returns>
         public static string BinarioDecimal(string binario)
         {
             if (Operando.EsBinario(binario))
@@ -113,70 +81,32 @@ namespace Entidades
             return auxNumero;
         }
 
-
-        /// <summary>
-        /// Metodo que valida que <param name="numero"></param> sea un numero binario
-        /// </summary>
-        /// <param name="numero"></param>
-        /// <returns>true si es binario, false si no</returns>
         private static bool EsBinario(string binario)
         {
             char[] cadena = binario.ToCharArray();
             bool retorno = false;
             for (int i = 0; i < binario.Length; i++)
             {
-                retorno = cadena[i] == '0' || cadena[i] == '1';
+                 retorno = cadena[i] == '0' || cadena[i] == '1';
             }
             return retorno;
         }
 
-        /// <summary>
-        /// sobrecarga del operador + para realizar operacion aritmetica entre 
-        /// dos objetos de tipo <see cref="Operando"/>
-        /// </summary>
-        /// <param name="n1"></param>
-        /// <param name="n2"></param>
-        /// <returns>Retorna la suma entre las propiedades <seealso cref="numero"/> de los objetos</returns>
         public static double operator +(Operando n1, Operando n2)
         {
             return n1.numero + n2.numero;
         }
 
-
-        /// <summary>
-        /// sobrecarga del operador - para realizar operacion aritmetica entre 
-        /// dos objetos de tipo <see cref="Operando"/>
-        /// </summary>
-        /// <param name="n1"></param>
-        /// <param name="n2"></param>
-        /// <returns>Retorna la resta entre las propiedades <seealso cref="numero"/> de los objetos</returns>
         public static double operator -(Operando n1, Operando n2)
         {
             return n1.numero - n2.numero;
         }
-
-
-        /// <summary>
-        /// sobrecarga del operador / para realizar operacion aritmetica entre 
-        /// dos objetos de tipo <see cref="Operando"/>
-        /// </summary>
-        /// <param name="n1"></param>
-        /// <param name="n2"></param>
-        /// <returns>Si <paramref name="n2"/> es 0, retorna <see cref="double.MinValue"/>, sino la division entre los dos numeros <seealso cref="numero"/> de los objetos</returns>
 
         public static double operator /(Operando n1, Operando n2)
         {
             return n2.numero == 0 ? double.MinValue : n1.numero / n2.numero;
         }
 
-
-        /// <summary>
-        /// sobrecarga del operador * para realizar operacion aritmetica entre 
-        /// dos objetos de tipo <see cref="Operando"/>
-        /// </summary>
-        /// <param name="n1"></param>
-        /// <param name="n2"></param>
-        /// <returns>Retorna la multiplicacion entre las propiedades <seealso cref="numero"/> de los objetos</returns>
         public static double operator *(Operando n1, Operando n2)
         {
             return n1.numero * n2.numero;
---
+++ RecuperatoriosTP/TP1/MiCalculadora/MiCalculadora/FormCalculadora.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+
+namespace MiCalculadora
+{
+    public partial class FormCalculadora : Form
+    {
+        public FormCalculadora()
+        {
+            InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// Metodo que se encarga de instanciar los dos operandos recibidos por parametros y de 
+        /// realizar la operacion acorde dependiendo del <paramref name="operador"/> recibido
+        /// </summary>
+        /// <param name="op1"></param>
+        /// <param name="op2"></param>
+        /// <param name="operador"></param>
+        /// <returns> retorna el resultado de la operacion aritmetica correspondiente</returns>
+        private static double Operar(string op1, string op2, string operador)
+        {
+            Operando operando1 = new Operando(op1);
+            Operando operando2 = new Operando(op2);
+            
+            return Calculadora.Operar(operando1, operando2, operador[0]);
+        }
+
+        /// <summary>
+        /// Metodo que valida que los caracteres ingresados en <paramref name="txtNumero"/> sean 
+        /// de caracter numerico
+        /// </summary>
+        /// <param name="txtNumero"></param>
+        /// <returns></returns>
+        private bool EsInputValido(TextBox txtNumero)
+        {
+            char[] cadenaCaracteres = txtNumero.Text.ToCharArray();
+            bool retorno = true;
+            foreach(char c in cadenaCaracteres)
+            {
+                retorno = char.IsDigit(c);
+            }
+            return retorno;
+        }
+
+        /// <summary>
+        /// Metodo que se encarga de limpiar <seealso cref="txtNumero1"/>, 
+        /// <seealso cref="txtNumero2"/>, 
+        /// <seealso cref="lstOperaciones"/> y 
+        /// <seealso cref="cmbOperador"/>
+        /// </summary>
+        private void Limpiar()
+        {
+            lstOperaciones.Items.Clear();
+            lblResultado.Text = "0";
+            txtNumero1.Text = "";
+            txtNumero2.Text = "";
+            cmbOperador.Text = "";
+        }
+
+        /// <summary>
+        /// Al momento del click, valida que los TextBox tengan un valor ingresado y realiza la operacion
+        /// aritmetica. En caso de que haya valores invalidos, muestra un componente MessageBox con un mensaje
+        /// de error.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtNumero1.Text) || string.IsNullOrWhiteSpace(txtNumero2.Text))
+            {
+                MessageBox.Show($"Debe ingresar ambos operandos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                string operador = string.IsNullOrWhiteSpace(cmbOperador.Text) ? "+" : cmbOperador.Text;
+               
+                double resultado = Operar(txtNumero1.Text, txtNumero2.Text, operador);
+                lblResultado.Text = resultado.ToString();
+                cmbOperador.Text = operador;
+                lstOperaciones.Items.Add(GetTextoOperacion());
+            }
+        }
+
+        /// <summary>
+        /// Metodo que maneja el evento TextChanged de <seealso cref="txtNumero1"/>
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtNumero1_TextChanged(object sender, EventArgs e)
+        {
+            if (!EsInputValido(txtNumero1))
+            {
+                txtNumero1.Text = "";
+            }
+        }
+
+        /// <summary>
+        /// Metodo que maneja el evento TextChanged de <seealso cref="txtNumero2"/>
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtNumero2_TextChanged(object sender, EventArgs e)
+        {
+            if (!EsInputValido(txtNumero2))
+            {
+                txtNumero2.Text = "";
+            }
+        }
+
+        /// <summary>
+        /// Maneja el evento que convierte el resultado de la operacion a binario, validando que haya previamente
+        /// una operacion realizada.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnConvertirABinario_Click(object sender, EventArgs e)
+        {
+
+            if (ValidarLblResultado())
+            {
+                MessageBox.Show($"Debe operar antes de intentar convertir", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                string resultado = Operando.DecimalBinario(lblResultado.Text);
+                this.SetResultadoConversion(lblResultado.Text, " en binario = ", resultado);
+            }
+        }
+
+        /// <summary>
+        /// Metodo que se encarga de manejar el evento de click para convertir el resultado de la operacion
+        /// en decimal
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnConvertirADecimal_Click(object sender, EventArgs e)
+        {
+            if (ValidarLblResultado())
+            {
+                MessageBox.Show($"Debe operar antes de intentar convertir", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                string resultado = Operando.BinarioDecimal(lblResultado.Text);
+                if(resultado == "Valor Invalido")
+                {
+                    MessageBox.Show(resultado, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    this.SetResultadoConversion(lblResultado.Text, " en decimal = ", resultado);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Maneja el evento que limpia <seealso cref="txtNumero1"/>, 
+        /// <seealso cref="txtNumero2"/>, 
+        /// <seealso cref="lstOperaciones"/> y 
+        /// <seealso cref="cmbOperador"/>, llamando a <seealso cref="Limpiar"/>
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            this.Limpiar();
+        }
+
+        /// <summary>
+        /// Boton que cierra el formulario, mostrando previamente un MessageBox para validar la eleccion
+        /// del usuario.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            DialogResult resultado = MessageBox.Show($"Seguro de querer salir?", "Error", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (DialogResult.Yes == resultado)
+            {
+                this.Close();
+            }
+        }
+
+
+        /// <summary>
+        /// Metodo que se encarga de llamar a <seealso cref="Limpiar"/>, poner focus en 
+        /// el TextBox de <seealso cref="txtNumero1"/> y setear los operadores en 
+        /// <seealso cref="cmbOperador"/>
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FormCalculadora_Load(object sender, EventArgs e)
+        {
+            this.Limpiar();
+            this.ActiveControl = txtNumero1;
+            cmbOperador.Items.Add("");
+            cmbOperador.Items.Add("+");
+            cmbOperador.Items.Add("-");
+            cmbOperador.Items.Add("/");
+            cmbOperador.Items.Add("*");
+        }
+
+        /// <summary>
+        /// Construye y devuelve dinamicamente el string que va a mostrarse en <seealso cref="lstOperaciones"/>
+        /// </summary>
+        /// <returns></returns>
+        private string GetTextoOperacion()
+        {
+            return txtNumero1.Text + " " + cmbOperador.Text + " " + txtNumero2.Text + " = " + lblResultado.Text;
+        }
+
+        /// <summary>
+        /// Valida el valor de <seealso cref="lblResultado"/>
+        /// </summary>
+        /// <returns></returns>
+        private bool ValidarLblResultado()
+        {
+            return string.IsNullOrWhiteSpace(lblResultado.Text);
+        }
+
+        /// <summary>
+        /// Setea el resultado correspondiente de la conversion realizada en los metodos 
+        /// <seealso cref="btnConvertirABinario_Click(object, EventArgs)"/> & 
+        /// <seealso cref="btnConvertirADecimal_Click(object, EventArgs)"/>
+        /// </summary>
+        /// <param name="numeroAConvertir"></param>
+        /// <param name="tipoConversion"></param>
+        /// <param name="numeroConvertido"></param>
+        private void SetResultadoConversion(string numeroAConvertir, string tipoConversion, string numeroConvertido)
+        {
+            string resultado = numeroAConvertir + tipoConversion + numeroConvertido;
+            lblResultado.Text = numeroConvertido;
+            lstOperaciones.Items.Add(resultado);
+        }
+    }
+}
